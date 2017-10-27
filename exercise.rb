0d8d9a55e526dd49f3c3aa9f6390fc471f4c4c13
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-27 10:22:01 -0400

    interates through standings and get a hash of the name of the cats and their scores

--- exercise.rb
+++ exercise.rb
@@ -5,7 +5,26 @@ ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
            {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
            {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
 
-standings = {}
-cats = ballots.map {|ballot| ballot.values }.flatten.uniq
-cats.each {|cat| standings[cat] = 0}
-puts standings
+
+def who_wins(ballots)
+  standings = {}
+
+  cats = ballots.map {|ballot| ballot.values }.flatten.uniq
+
+  cats.each { |cat| standings[cat] = 0 }
+  standings
+
+  standings.each do |cat, score|
+    ballots.each do |ballot|
+      ballot.each do |k, v|
+        if cat == v
+          standings[cat] += k
+        end
+      end
+    end
+  end
+  p standings
+  standings.values.max(3)
+end
+
+p who_wins(ballots)
